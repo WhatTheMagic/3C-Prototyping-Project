4f25f81008e09ec6fa63829488a9be2d333992df
Grengidone <wezeman.arthur@gmail.com>
2022-10-04 18:26:18 +0200

    Slighty More functioning spawner

--- 3C Prototyping Project/Assets/Scripts/Mangaers/SpawnerScript.cs
+++ 3C Prototyping Project/Assets/Scripts/Mangaers/SpawnerScript.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,51 +13,51 @@ public class SpawnerScript : MonoBehaviour
     
     // Store childrens colliders
     Collider[] colliders;
+    [SerializeField] List<float> colliderArea = new List<float>();
 
     #region Vector3 collider areas 
     Vector3 spawnPoint;
-    Vector3 m_Center;
-    Vector3 m_Size, m_Min, m_Max;
     #endregion
 
     // The total area the colliders covers, relative to the (X,Z)-Axis
-    float spawnArea;
+    float spawnTotalArea;
 
     void Start()
     {
         colliders = GetComponentsInChildren<Collider>();
-        //Fetch the Collider from the GameObject
-        
-        //Fetch the center of the Collider volume
-        m_Center = colliders[0].bounds.center;
-        //Fetch the size of the Collider volume
-        m_Size = colliders[0].bounds.size;
-        //Fetch the minimum and maximum bounds of the Collider volume
-        m_Min = colliders[0].bounds.min;
-        m_Max = colliders[0].bounds.max;
-        //Output this data into the console
+        Debug.LogAssertion(colliders[0].bounds.center);
+        Debug.LogAssertion(colliders[0].bounds.size);
+
         OutputData();
     }
 
     private void OutputData()
     {
-        //Output to the console the center and size of the Collider volume
-        Debug.Log("Collider Center : " + m_Center);
-        Debug.Log("Collider Size : " + m_Size);
-        Debug.Log("Collider bound Minimum : " + m_Min);
-        Debug.Log("Collider bound Maximum : " + m_Max);
 
-        spawnArea = 0;
+        spawnTotalArea = 0;
         foreach(var collider in colliders)
         {
-            float size = collider.bounds.size.x * collider.bounds.size.z;
-            spawnArea += size;
+            float size = 0;
+            switch (collider)
+            {
+                case BoxCollider:
+                    size = collider.bounds.size.x * collider.bounds.size.z;
+                    break;
+                case SphereCollider:
+                    SphereCollider col = collider as SphereCollider;
+                    size = Mathf.Pow(col.radius * CompareLocalScale(col.transform.localScale), 2f) * Mathf.PI;
+                    break;
+                default:
+                    break;
+            }
+ 
+            spawnTotalArea += size;
+            colliderArea.Add(size);
             Debug.Log(size);
         }
-        Debug.LogAssertion(spawnArea);
+        Debug.LogAssertion(spawnTotalArea);
 
         
-        Spawn(spawnItem);
         StartCoroutine(Poop());
         
     }
@@ -73,15 +74,32 @@ public class SpawnerScript : MonoBehaviour
 
     }
 
+    float CompareLocalScale(Vector3 localScale)
+    {
+        float x = Mathf.Abs(localScale.x);
+        float y = Mathf.Abs(localScale.y);
+        float z = Mathf.Abs(localScale.z);
+        if (x >= y && x >= z)
+        {
+            return x;
+        } else if (y >= x && y >= z) 
+        {
+            return y;
+        } else
+        {
+            return z;
+        }
+    }
+    
     [ContextMenu("Poop")]
     void Spawn()
     {
-        float random = UnityEngine.Random.Range(0, spawnArea);
+        float random = UnityEngine.Random.Range(0, spawnTotalArea);
         float collect = 0;
         Collider col = null;
-        for (int i = 0; i < colliders.Length; i++)
+        for (int i = 0; i < colliderArea.Count; i++)
         {
-            float size = colliders[i].bounds.size.x * colliders[i].bounds.size.z;
+            float size = colliderArea[i];
             collect += size;
             if (collect >= random)
             {
@@ -91,7 +109,22 @@ public class SpawnerScript : MonoBehaviour
             }
 
         }
-        spawnPoint = new Vector3(UnityEngine.Random.Range(col.bounds.min.x, col.bounds.max.x), 1f, UnityEngine.Random.Range(col.bounds.min.z, col.bounds.max.z));
+        
+        switch (col)
+        {
+            case BoxCollider:
+                 spawnPoint = new Vector3(UnityEngine.Random.Range(-col.bounds.size.x, col.bounds.size.z) / 2 , 0f, UnityEngine.Random.Range(-col.bounds.size.z, col.bounds.size.z) / 2) + col.bounds.center;
+
+                break;
+
+            case SphereCollider:
+                SphereCollider collider = (SphereCollider)col;
+                spawnPoint = UnityEngine.Random.Range(0f, collider.radius *  CompareLocalScale(col.transform.localScale)) * new Vector3(UnityEngine.Random.Range(-10f, 10f), 0f, UnityEngine.Random.Range(-10f, 10f)).normalized + col.bounds.center;
+
+                break;
+            default:
+                break;
+        }
         RaycastHit hit;
         if (Physics.Raycast(spawnPoint, Vector3.down, out hit, Mathf.Infinity))
         {
@@ -101,7 +134,7 @@ public class SpawnerScript : MonoBehaviour
     }
      void Spawn(GameObject spawnObject)
     {
-        float random = UnityEngine.Random.Range(0, spawnArea);
+        float random = UnityEngine.Random.Range(0, spawnTotalArea);
         float collect = 0;
         Collider col = null;
         for (int i = 0; i < colliders.Length; i++)
